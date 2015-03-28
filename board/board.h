cacfab89139511c1624033d4835fcee78468e8f1
Michael Spieler <the.chaotic.coder@gmail.com>
2015-03-28 17:08:22 +0100

    Use 16MHz HSE.

--- board/board.h
+++ board/board.h
@@ -38,11 +38,7 @@
 
 #define STM32_LSEDRV                (3 << 3)
 
-#if !defined(STM32_HSECLK)
-#define STM32_HSECLK                0
-#endif
-
-#define STM32_HSE_BYPASS
+#define STM32_HSECLK                16000000
 
 /*
  * MCU type as defined in the ST header.
