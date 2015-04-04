e88fc26b7afc73b91e61709937ba9ddac5ccfef0
Michael Spieler <the.chaotic.coder@gmail.com>
2015-04-04 13:38:15 +0200

    Fix MPU60X0 driver.

--- src/mpu60X0.c
+++ src/mpu60X0.c
@@ -4,20 +4,25 @@
 #include "mpu60X0.h"
 #include "mpu60X0_registers.h"
 
+#define STANDARD_GRAVITY 9.80665f
+#define DEG2RAD(deg) (deg/180*M_PI)
 
 static uint8_t mpu60X0_reg_read(mpu60X0_t *dev, uint8_t reg)
 {
     uint8_t ret = 0;
     if (dev->spi) {
-        // spiSelect(dev->spi);
-        // reg |= 0x80;
-        // spiSend(dev->spi, 1, &reg);
-        // spiReceive(dev->spi, 1, &ret);
-        // spiUnselect(dev->spi);
-    } else if (dev->i2c_driver) {
-        I2CDriver *driver = dev->i2c_driver;
+#if HAL_USE_SPI
+        spiSelect(dev->spi);
+        reg |= 0x80;
+        spiSend(dev->spi, 1, &reg);
+        spiReceive(dev->spi, 1, &ret);
+        spiUnselect(dev->spi);
+#endif
+    } else if (dev->i2c) {
+#if HAL_USE_I2C
         uint8_t addr = dev->i2c_address;
-        i2cMasterTransmit(driver, addr, &reg, 1, &ret, 1);
+        i2cMasterTransmit(dev->i2c, addr, &reg, 1, &ret, 1);
+#endif
     }
     return ret;
 }
@@ -26,53 +31,50 @@ static void mpu60X0_reg_write(mpu60X0_t *dev, uint8_t reg, uint8_t val)
 {
     uint8_t buf[] = {reg, val};
     if (dev->spi) {
-        // spiSelect(dev->spi);
-        // spiSend(dev->spi, 2, buf);
-        // spiUnselect(dev->spi);
-    } else if (dev->i2c_driver) {
-        I2CDriver *driver = dev->i2c_driver;
+#if HAL_USE_SPI
+        spiSelect(dev->spi);
+        spiSend(dev->spi, 2, buf);
+        spiUnselect(dev->spi);
+#endif
+    } else if (dev->i2c) {
+#if HAL_USE_I2C
         uint8_t addr = dev->i2c_address;
-        i2cMasterTransmit(driver, addr, buf, 2, NULL, 0);
+        i2cMasterTransmit(dev->i2c, addr, buf, 2, NULL, 0);
+#endif
     }
 }
 
 static void mpu60X0_reg_read_multi(mpu60X0_t *dev, uint8_t reg, uint8_t *buf, int8_t len)
 {
     if (dev->spi) {
-        // spiSelect(dev->spi);
-        // reg |= 0x80;
-        // spiSend(dev->spi, 1, &reg);
-        // spiReceive(dev->spi, len, buf);
-        // spiUnselect(dev->spi);
-    } else if (dev->i2c_driver) {
-        I2CDriver *driver = dev->i2c_driver;
+#if HAL_USE_SPI
+        spiSelect(dev->spi);
+        reg |= 0x80;
+        spiSend(dev->spi, 1, &reg);
+        spiReceive(dev->spi, len, buf);
+        spiUnselect(dev->spi);
+#endif
+    } else if (dev->i2c) {
+#if HAL_USE_I2C
         uint8_t addr = dev->i2c_address;
-        i2cMasterTransmit(driver, addr, &reg, 1, buf, len);
+        i2cMasterTransmit(dev->i2c, addr, &reg, 1, buf, len);
+#endif
     }
 }
 
-#if 0
-static void mpu60X0_reg_write_multi(mpu60X0_t *dev, uint8_t reg, const uint8_t *buf, int8_t len)
+#if HAL_USE_SPI
+void mpu60X0_init_using_spi(mpu60X0_t *dev, SPIDriver *spi_dev)
 {
-    if (dev->spi) {
-        spiSelect(dev->spi);
-        spiSend(dev->spi, 1, &reg);
-        spiSend(dev->spi, len, buf);
-        spiUnselect(dev->spi);
-    }
+    dev->spi = spi_dev;
+    dev->i2c = NULL;
+    dev->config = 0;
 }
 #endif
 
-// void mpu60X0_init_using_spi(mpu60X0_t *dev, SPIDriver *spi_dev)
-// {
-//     dev->spi = spi_dev;
-//     dev->i2c_driver = NULL;
-//     dev->config = 0;
-// }
-
+#if HAL_USE_I2C
 void mpu60X0_init_using_i2c(mpu60X0_t *dev, I2CDriver *i2c_dev, int ad0_pin_value)
 {
-    dev->i2c_driver = i2c_dev;
+    dev->i2c = i2c_dev;
     dev->spi = NULL;
     dev->config = 0;
     if (ad0_pin_value == 0) {
@@ -81,6 +83,7 @@ void mpu60X0_init_using_i2c(mpu60X0_t *dev, I2CDriver *i2c_dev, int ad0_pin_valu
         dev->i2c_address = 0x69;
     }
 }
+#endif
 
 void mpu60X0_setup(mpu60X0_t *dev, int config)
 {
@@ -135,8 +138,14 @@ static int32_t read_word(const uint8_t *buf) // signed int16
 
 void mpu60X0_read(mpu60X0_t *dev, float *gyro, float *acc, float *temp)
 {
-    static const float gyro_res[] = {(1/131.f)/180*M_PI, (1/65.5f)/180*M_PI, (1/32.8f)/180*M_PI, (1/16.4f)/180*M_PI}; // rad/s/LSB
-    static const float acc_res[] = {1/16384.f, 1/8192.f, 1/4096.f, 1/2048.f}; // m/s^2 / LSB
+    static const float gyro_res[] = { DEG2RAD(1/131.f),
+                                      DEG2RAD(1/65.5f),
+                                      DEG2RAD(1/32.8f),
+                                      DEG2RAD(1/16.4f) }; // rad/s/LSB
+    static const float acc_res[] = { STANDARD_GRAVITY/16384.f,
+                                     STANDARD_GRAVITY/8192.f,
+                                     STANDARD_GRAVITY/4096.f,
+                                     STANDARD_GRAVITY/2048.f }; // m/s^2 / LSB
     uint8_t buf[1 + 6 + 2 + 6]; // interrupt status, accel, temp, gyro
     mpu60X0_reg_read_multi(dev, MPU60X0_RA_INT_STATUS, buf, sizeof(buf));
     if (acc) {
@@ -145,7 +154,7 @@ void mpu60X0_read(mpu60X0_t *dev, float *gyro, float *acc, float *temp)
         acc[2] = (float)read_word(&buf[5]) * acc_res[dev->config & 0x3];
     }
     if (temp) {
-        *temp = (float)read_word(&buf[7]) / 340 + 36.53;
+        *temp = (float)read_word(&buf[7]) / 340.0f + 36.53f;
     }
     if (gyro) {
         gyro[0] = (float)read_word(&buf[9]) * gyro_res[(dev->config >> 2) & 0x3];
