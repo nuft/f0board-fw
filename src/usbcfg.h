60664b4f4a182257318f7b0ff8b910d46b13148e
Michael Spieler <michael.spieler@epfl.ch>
2015-10-13 10:42:36 +0200

    update usbcfg

--- src/usbcfg.h
+++ src/usbcfg.h
@@ -17,11 +17,9 @@
 #ifndef _USBCFG_H_
 #define _USBCFG_H_
 
-#include "hal.h"
-
-extern SerialUSBDriver SDU1;
 extern const USBConfig usbcfg;
-extern const SerialUSBConfig serusbcfg;
+extern SerialUSBConfig serusbcfg1;
+extern SerialUSBConfig serusbcfg2;
 
 #endif  /* _USBCFG_H_ */
 
